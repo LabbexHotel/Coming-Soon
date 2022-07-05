505029e968bbaef6d9eb4352f441c46835b4b2ea
Ruxwez <47431411+Ruxwez@users.noreply.github.com>
2022-07-05 22:46:39 +0200

    .

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+export default () => {
+  return (
+    <div className="Proximamente">
+      <div className="Container">
+        <img
+          src="https://cdn.jsdelivr.net/gh/LabbexHotel/CDN@main/LogoCompuesto.png"
+          alt="Logo"
+        />
+        <h1>Proximamente</h1>
+        <h2>Actualmente nos encontramos en fase de desarrollo</h2>
+        <div className="ButtonsSocial"></div>
+      </div>
+      <a className="ButtonPowered" href="https://pixidev.com" target="_blank" rel="noopener noreferrer">
+        Powered Pixi<span>dev</span>
+      </a>
+    </div>
+  );
+};
